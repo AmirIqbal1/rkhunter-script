4c4cef804b514daca429c0c6dd203f3c7588b233
Amir <AmirIqbal1@users.noreply.github.com>
2020-04-28 11:11:45 +0100

    updated command
    updated so only shows errors

--- rkhunter-check.sh
+++ rkhunter-check.sh
@@ -6,7 +6,7 @@ rkhunter --update
 #Set the Security Baseline for your system
 rkhunter --propupd
 
-#checks for rootkits without keypress needed
-rkhunter --check --sk
+#checks for rootkits without keypress needed & shows only warnings
+rkhunter --check --sk --rwo
 
 #all logs are saved to /var/log/rkhunter.log
